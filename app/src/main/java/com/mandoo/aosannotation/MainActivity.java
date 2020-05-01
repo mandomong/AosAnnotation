8e59f101ec963d44f286c88510530f7f845e98b0
mandoomong <orangetreex@gmail.com>
2020-05-01 14:31:59 +0900

    Initial commit

---
+++ app/src/main/java/com/mandoo/aosannotation/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mandoo.aosannotation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.myInput)
+    EditText myInput;
+
+    @ViewById(R.id.myTextView)
+    TextView textView;
+
+    @Click
+    void myButton() {
+        String name = myInput.getText().toString();
+        textView.setText("Hello "+name);
+    }
+}
